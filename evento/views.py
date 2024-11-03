f3afbf2e4465fd06b741e480444ff4c671b87795
Nilson Pagnez <nilson.pagneto@gmail.com>
2024-11-03 19:07:06 -0300

    ajustando css´s

--- evento/views.py
+++ evento/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Evento
-from django.views.generic import ListView
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
 
@@ -21,8 +20,12 @@ class EventoListView(ListView):
 class EventoCreateView(CreateView):
     model = Evento
     fields = ["titulo", "data", "local", "descricao", "idCriador"]
-
-class TodoCreateView(CreateView):
-    model = Evento
-    fields = ["titulo", "data", "local", "descricao", "idCriador"]
     success_url = reverse_lazy("evento_list")
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        # Adiciona a classe 'form-control' a todos os inputs, textareas e selects
+        for field in form.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})  # ou outra classe desejada
+        return form
+
